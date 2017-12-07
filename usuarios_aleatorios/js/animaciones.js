893c933ac7a4be7d27d29140f84e87f4363e29fc
endikamanoso <triliton@hotmail.com>
2017-12-07 12:57:36 +0100

    tercer commit - introduccion primeros datos en la web

--- usuarios_aleatorios/js/animaciones.js
+++ usuarios_aleatorios/js/animaciones.js
@@ -9,6 +9,10 @@ $(function () {
             "display": "none",
         })
         console.log(jqxhr.responseJSON)
+
+        $(jqxhr.responseJSON.results).each(function (indice,valor) {
+            $("body").append("<p><img src='"+valor.picture.thumbnail+"'></img>"+valor.name.first+" "+valor.name.last+"</p>")
+        })
         
     })
 
