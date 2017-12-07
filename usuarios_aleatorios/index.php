619c32e8c1eaada49a87d05058cc4ada506ec987
endikamanoso <triliton@hotmail.com>
2017-12-07 12:39:45 +0100

    primer commit- boton desaparece

---
+++ usuarios_aleatorios/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Usuarios Aleatorios</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container text-center">
+  <div class="jumbotron text-center">
+    <h1>Lista aleatoria de Usuarios</h1> 
+  </div>
+  <button id="boton" class="btn btn-primary col-9">Hazlo!</button>
+        <br><br>
+  <div id="contenido"></div>
+</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.2.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="js/animaciones.js"></script>
+  </body>
+</html>
---
+++ usuarios_aleatorios/js/animaciones.js
@@ -0,0 +1,16 @@
+$(function () {
+    jqxhr=$.ajax({
+        url: 'https://randomuser.me/api/?results=50',
+        dataType:'json'
+    });
+
+    $("button").on("click", function () {
+        $(this).css({
+            "display": "none",
+        })
+        $(jqxhr.responseJSON.results).each(function (indice,valor) {
+            $("body").append("<p><img src='"+valor.picture.thumbnail+"'></img>"+valor.name.first+" "+valor.name.last+"</p>")
+        })
+    })
+
+})
